89f86dbcd945346b7461e5928bd935f30d490feb
care-code <info@care-code.site>
2022-03-07 22:50:41 +0900

    vue.jsの学習

--- vue/index.js
+++ vue/index.js
@@ -1,13 +1,7 @@
-var app = new Vue({
+let app = new Vue({
   el: "#app",
   data: {
-    message: "ラッキー",
-  },
-});
-
-var app2 = new Vue({
-  el: "#app2",
-  date: {
-    message: "これはapp2になります",
+    error_class: "error",
+    img_src: "img02.png",
   },
 });

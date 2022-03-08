b036f8f9f8f77cb3df969237b9693d4f9c50667b
care-code <info@care-code.site>
2022-03-08 13:08:59 +0900

    study20220308

--- vue/index.js
+++ vue/index.js
@@ -1,7 +1,63 @@
+/**
+ * time.htmlのjs
+ */
 let app = new Vue({
   el: "#app",
   data: {
-    error_class: "error",
-    img_src: "img02.png",
+    now: "00:00:00",
   },
+  methods: {
+    time: function (e) {
+      var date = new Date();
+      this.now =
+        date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    },
+  },
+});
+
+/**
+ * for.htmlのjs
+ */
+
+let app02 = new Vue({
+  el: "#app02",
+  data: {
+    prefs: [
+      { name: "北海道" },
+      { name: "青森" },
+      { name: "岩手" },
+      { name: "宮城" },
+      { name: "山形" },
+      { name: "秋田" },
+    ],
+  },
+  /**
+   * animation.htmlのjs
+   */
+  methods: {
+    shuffle: function () {
+      this.prefs = _.shuffle(this.prefs);
+    },
+  },
+});
+
+/**
+ * compornents.htmlのjs
+ */
+Vue.component("alert-box", {
+  template: `
+  <div class="alert" v-on:click="caution">
+  <strong>ERROR!</strong>
+  <slot></slot>
+  </div>
+  `,
+  methods: {
+    caution: function () {
+      alert("クリックされました");
+    },
+  },
+});
+
+let app3 = new Vue({
+  el: "#app03",
 });
